972d1381eaf2aa9e5147b157890877e52f5d893a
Vitor Delgado <vitor.ldelgado@gmail.com>
2022-05-26 18:47:38 -0300

    Alteração 4 26/05

--- src/Objetos/ArmazenaDados.java
+++ src/Objetos/ArmazenaDados.java
@@ -6,13 +6,20 @@ import java.util.List;
 public class ArmazenaDados {
 
         protected static List<Cliente> listaClientes = new ArrayList<>();
-        protected static List<Bebida> listaBebidas = new ArrayList<>();
+        //protected static List<Bebida> listaBebidas = new ArrayList<>();
         protected static List<Pizza> listaPizzas = new ArrayList<>();
         protected static List<Sobremesa> listaSobremesas = new ArrayList<>();
         protected static List <Pedido> pedidosTemp = new ArrayList<>();
+        protected static ArrayList <String> bairros = new ArrayList<>();
 
 
+        public static void criarBairros(){
+                bairros.add("vila formosa");
 
-
-
+        }
+        public static void imprimirBairros(){
+                for (int i = 0; i < bairros.size(); i++) {
+                        System.out.println(bairros.get(i));
+                }
+        }
 }
